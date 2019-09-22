75faf0b5e14c5667f594ebd12fcddddc3a960239
Alexandr Kanishevskiy <a.kan4988@gmail.com>
2019-09-22 20:08:38 +0300

    add new test GroupModificationTests

---
+++ addressbook-web-tests/src/test/java/ru/stqa/qajava/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.qajava.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.qajava.addressbook.model.GroupData;
+
+public class GroupModificationTests extends  TestBase{
+  @Test
+  public void testGroupModification()throws Exception{
+    app.getNavigationHelper().gotoGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupFrom(new GroupData("test1", "test2", "test23"));
+    app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returnToGroupPage();
+  }
+
+}
