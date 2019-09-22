9721ff13ea47f6bbdcc7d62e03eebeea8ee9ca02
Alexandr Kanishevskiy <a.kan4988@gmail.com>
2019-09-22 19:47:14 +0300

    add method click whis locator like parametr

--- addressbook-web-tests/src/test/java/ru/stqa/qajava/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/qajava/addressbook/appmanager/ApplicationManager.java
@@ -41,14 +41,7 @@ public class ApplicationManager {
     }
   }
 
-  private boolean isAlertPresent() {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
+
 
   private String closeAlertAndGetItsText() {
     try {
