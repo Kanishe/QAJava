9ce99f93f094b2e484b8d59b8096d6262f557401
Alexandr Kanishevskiy <a.kan4988@gmail.com>
2019-11-17 14:43:14 +0300

    38 ЦиклыМногократные повторения похожих действий.mp4
     38 ЦиклыМногократные повторения похожих действий.mp4

---
+++ sandbox/src/test/java/PrimeTests.java
@@ -0,0 +1,9 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PrimeTests {
+  @Test
+  public void testPrime(){
+    Assert.assertTrue (Primes.isPrime(Integer.MAX_VALUE));
+  }
+}
