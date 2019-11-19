3d469bda945c82b04492ceaa6da85c96989fa51f
Alexandr Kanishevskiy <a.kan4988@gmail.com>
2019-11-19 23:26:29 +0300

    40 Количество элементов в коллекции.mp4
    40 Количество элементов в коллекции.mp4
    - Добавил метод проверки кол-ва элементов на странице до и после выполнения теста

--- addressbook-web-tests/src/test/java/ru/stqa/qajava/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/qajava/addressbook/tests/GroupCreationTest.java
@@ -1,5 +1,6 @@
 package ru.stqa.qajava.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.qajava.addressbook.model.GroupData;
 
@@ -9,9 +10,13 @@ public class GroupCreationTest extends TestBase{
   @Test
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
+    int before=app.getGroupHelper().GetGroupCount();
     app.getGroupHelper().initGroupCreation();
     app.getGroupHelper().fillGroupForm(new GroupData("te", null, null));
     app.getGroupHelper().submitGroupCreation();
     app.getGroupHelper().reternToGroupPage();
+    int after=app.getGroupHelper().GetGroupCount();
+    Assert.assertEquals(after, before+1);
+
   }
 }
