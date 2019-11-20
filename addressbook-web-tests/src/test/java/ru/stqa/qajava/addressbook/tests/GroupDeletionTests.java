72fce1b2167f639335d13473a211bf05695ab7fa
Alexandr Kanishevskiy <a.kan4988@gmail.com>
2019-11-20 23:32:49 +0300

    lil fix
    lil fix

--- addressbook-web-tests/src/test/java/ru/stqa/qajava/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/qajava/addressbook/tests/GroupDeletionTests.java
@@ -9,10 +9,12 @@ public class GroupDeletionTests extends TestBase {
   @Test
   public void testGroupDeletion() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
+    int begin=app.getGroupHelper().GetGroupCount();
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().reternToGroupPage();
-
+    int after=app.getGroupHelper().GetGroupCount();
+    Assert.assertEquals(after, begin-1);
   }
 
 }
