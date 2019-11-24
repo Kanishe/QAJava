eebfcc02016e585be3465a45db5b15e70025b6c8
Alexandr Kanishevskiy <a.kan4988@gmail.com>
2019-11-24 15:57:08 +0300

    42 Формирование коллекции.
    42 Формирование коллекции.
    
    Метод для создания списка элементов getGroupList

--- addressbook-web-tests/src/test/java/ru/stqa/qajava/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/qajava/addressbook/tests/GroupCreationTest.java
@@ -5,19 +5,21 @@ import org.testng.annotations.Test;
 import ru.stqa.qajava.addressbook.appmanager.GroupHelper;
 import ru.stqa.qajava.addressbook.model.GroupData;
 
+import java.util.List;
+
 
 public class GroupCreationTest extends TestBase{
 
   @Test
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
+    List<GroupData>before=app.getGroupHelper().getGroupList();
     GroupHelper groupHelper=app.getGroupHelper();
-    int before=groupHelper.GetGroupCount();
     groupHelper.initGroupCreation();
     groupHelper.fillGroupForm(new GroupData("test40", "40", "40"));
     groupHelper.submitGroupCreation();
     groupHelper.reternToGroupPage();
-    int after=groupHelper.GetGroupCount();
-    Assert.assertEquals(after, before+1);
+    List<GroupData>after=app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size()+1);
   }
 }
