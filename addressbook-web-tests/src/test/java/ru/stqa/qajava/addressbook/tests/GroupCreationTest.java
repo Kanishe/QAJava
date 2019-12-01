792e9174883085d26263e1fb264c392bea91c206
Alexandr Kanishevskiy <a.kan4988@gmail.com>
2019-12-01 21:25:30 +0300

    46 Лямбдавыражения анонимные функции и элементы функционального программирования.mp4
    46 Лямбдавыражения анонимные функции и элементы функционального программирования.mp4

--- addressbook-web-tests/src/test/java/ru/stqa/qajava/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/qajava/addressbook/tests/GroupCreationTest.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import ru.stqa.qajava.addressbook.appmanager.GroupHelper;
 import ru.stqa.qajava.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -15,21 +16,25 @@ public class GroupCreationTest extends TestBase{
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData>before=app.getGroupHelper().getGroupList();
-    GroupData group = new GroupData("test1",null,null);
+    GroupData group = new GroupData("test2",null,null);
     app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size()+1);
 
+// Сравнение циклом
+//    int max = 0;
+//    for (GroupData g : after){
+//      if (g.getId() > max){
+//        max = g.getId();
+//      }
+//    }
 
-    int max = 0;
-    for (GroupData g : after){
-      if (g.getId() > max){
-        max = g.getId();
-      }
-    }
-    group.setId(max);
+/** лямбда выражения для сравниения id  вместо обычного цыкла
+*/
+
+ group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId());
     before.add(group);
-    Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(before));//todo хот фикс правера не правильная
+    Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));//todo хот фикс правера не правильная
   }
 
 }
