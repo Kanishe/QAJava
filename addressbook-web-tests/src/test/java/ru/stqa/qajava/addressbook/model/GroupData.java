50ac2472134f49fd4b09388153b67b849a024cd6
Alexandr Kanishevskiy <a.kan4988@gmail.com>
2019-12-01 16:57:26 +0300

    43 Сравнение коллекций.mp4
    43 Сравнение коллекций.mp4

--- addressbook-web-tests/src/test/java/ru/stqa/qajava/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/qajava/addressbook/model/GroupData.java
@@ -5,10 +5,32 @@ public class GroupData {
   private final String groupHeader;
   private final String groupFooter;
 
+  @Override
+  public String toString() {
+    return "GroupData{" +
+            "groupName='" + groupName + '\'' +
+            '}';
+  }
+
   public GroupData(String groupName, String groupHeader, String groupFooter) {
     this.groupName = groupName;
     this.groupHeader = groupHeader;
     this.groupFooter = groupFooter;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    GroupData groupData = (GroupData) o;
+
+    return groupName != null ? groupName.equals(groupData.groupName) : groupData.groupName == null;
+  }
+
+  @Override
+  public int hashCode() {
+    return groupName != null ? groupName.hashCode() : 0;
   }
 
   public String getGroupName() {
